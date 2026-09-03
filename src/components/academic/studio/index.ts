014808af89a67b4bd129152efa4e2979ae73bc87
Paulo Borges Junior <contatopborgesj@gmail.com>
2026-09-03 14:39:51 -0300

    feat(academic-studio): implement obsidian-style sidebar, canvas editor, and studio container (#12)

---
+++ src/components/academic/studio/index.ts
@@ -0,0 +1,8 @@
+export { AcademicStudio } from './AcademicStudio'
+export type { AcademicStudioProps } from './AcademicStudio'
+
+export { StudioSidebar } from './StudioSidebar'
+export type { StudioSidebarProps } from './StudioSidebar'
+
+export { StudioEditor } from './StudioEditor'
+export type { StudioEditorProps } from './StudioEditor'
